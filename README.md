e1a5c2ffc23e36d8c4be2cfae90e78a6d7b03000
serj-kzv <serj.kzv@gmail.com>
2019-10-29 12:13:16 +0300

    it is not done yet

---
+++ README.md
@@ -0,0 +1,23 @@
+# How to use
+**Example 1.** Run after page is loaded and track all DOM changes.
+```js
+const elementMutator = new ElementMutator({attributeFilter: []}, (cfg, node, attributeName) => {
+    // do something
+    elementMutator.stop(); // stop
+});
+```
+**Example 2.** Track a concreate DOM node and all his descendants.
+```js
+const elementMutator = new ElementMutator({attributeFilter: [], node: document.body}, (cfg, node, attributeName) => {
+    // do something
+    elementMutator.stop(); // stop
+});
+```
+**Example 3.** Track a concreate DOM node attributes.
+If `attributeName` is `undefined` then mutation is a non attribute mutation.
+```js
+const elementMutator = new ElementMutator({attributeFilter: [href]}, (cfg, node, attributeName) => {
+    // do something
+    elementMutator.stop(); // stop
+});
+```
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const distDir = path.resolve(__dirname, 'dist');
+const srcDir = path.resolve(__dirname, 'src');
+const plugins = [
+    new CopyWebpackPlugin(
+        [
+            {
+                from: '**/!(*.js)',
+                to: distDir,
+                context: srcDir,
+                ignore: []
+            }
+        ],
+        {
+            copyUnmodified: false
+        }
+    )
+];
+const cfg = {
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    node: {fs: 'empty'},
+    // devtool: options.devtool,
+    target: 'web', // Make web variables accessible to webpack, e.g. window
+    // performance: options.performance || {},
+    // optimization: {
+    //     minimize: false
+    // }
+    watchOptions: {
+        ignored: ['node_modules'],
+        poll: 500
+    }
+};
+
+module.exports = (env, argv) => {
+    let backgroundPlugins = [...plugins];
+
+    if (argv.mode === 'production') {
+        backgroundPlugins.unshift(new CleanWebpackPlugin());
+        cfg.optimization = {
+            // https://github.com/webpack-contrib/terser-webpack-plugin
+            minimizer: [new TerserPlugin({
+                sourceMap: false,
+                parallel: true,
+                cache: true,
+                // https://github.com/terser-js/terser#minify-options
+                terserOptions: {
+                    ecma: 8,
+                    compress: {
+                        drop_console: true,
+                        drop_debugger: true // TODO: find out why it does not work
+                    }
+                }
+            })]
+        };
+    }
+
+    return [
+        {
+            entry: './src/ElementMutator.js',
+            output: {
+                path: distDir,
+                filename: 'ElementMutator.js'
+            },
+            plugins: backgroundPlugins,
+            ...cfg
+        },
+    ];
+};
